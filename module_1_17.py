c3fa82253eaec27a938ad0111c58da2df5d767f7
Billy <iceqlips@gmail.com>
2024-12-06 20:28:02 +0500

    Update module_1_17.py

--- module_1_17.py
+++ module_1_17.py
@@ -1,6 +1,6 @@
 immutable_var = 1, 0.5, 'String'
 print(immutable_var)
-# immutable_var[1] = 1.5 # tuple то есть кортеж, не принял изменение объекта, потому что в отличии от списка, кортеж неизменяем.
+# immutable_var[1] = 1.5 # tuple то есть кортеж, не принял изменение объекта внутри кортежа, потому что в отличии от списка, кортеж неизменяем.
 mutable_list = [10, 5.5, 'String']
 mutable_list.extend(['String2'])
 print(mutable_list)
